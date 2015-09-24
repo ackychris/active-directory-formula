903f731fd6ebdfb059d0654ba6c2dcf9b1ea0af6
Matthew X. Economou <xenophon@irtnog.org>
2015-09-24 09:53:39 -0400

    fix(salt.grains.windows_installation_type): query WMI directly instead of relying on core grains
    The `osfullname` grain isn't accessible from within the custom grain, so
    the operating system information must be retrieved directly from Windows
    Management Instrumentation (WMI).  The code here is copied from
    *salt.grains.core._windows_platform_data()*.

--- _grains/windows_installation_type.py
+++ _grains/windows_installation_type.py
@@ -1,18 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import logging
-from os import environ, path, sep
-from re import match
+import os
+import re
+import salt.utils
 
 log = logging.getLogger(__name__)
 
+HAS_WMI = False
+if salt.utils.is_windows():
+    try:
+        import wmi
+        import salt.utils.winapi
+        HAS_WMI = True
+    except ImportError:
+        log.exception('Unable to import Python wmi module; the osinstalltype grain will be missing.')
+
 def windows_installation_type():
+    if not HAS_WMI:
+        return {}
+
+    with salt.utils.winapi.Com():
+        wmi_c = wmi.WMI()
+        osinfo = wmi_c.Win32_OperatingSystem()[0]
+        (osfullname, _) = osinfo.Name.split('|', 1)
+        osfullname = osfullname.strip()
+
     ## A simple hack---"%windir%\explorer.exe" does not exist on
     ## Server Core.  This will need to be revised with the release of
     ## vNext Server, which adds a "Nano Server" install type.
     grains = {}
-    if match('Microsoft Windows Server', __grains__['osfullname']):
-        if path.isfile(sep.join([environ['windir'], "explorer.exe"])):
+    if re.match('Microsoft Windows Server', osfullname):
+        if os.path.isfile(
+                os.sep.join(
+                    [os.environ['windir'],
+                     "explorer.exe"])):
             grains['osinstalltype'] = 'Standard'
         else:
             grains['osinstalltype'] = 'Server Core'
